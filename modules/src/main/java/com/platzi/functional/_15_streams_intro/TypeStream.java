4b3891ef61c03d07d6a7a54d9175e2d37034d3cc
Luis Angel <angelfgdeveloper@gmail.com>
2022-12-17 13:22:56 -0600

    Uso de collectors

--- modules/src/main/java/com/platzi/functional/_15_streams_intro/TypeStream.java
+++ modules/src/main/java/com/platzi/functional/_15_streams_intro/TypeStream.java
@@ -1,5 +1,7 @@
 package com.platzi.functional._15_streams_intro;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class TypeStream {
@@ -13,6 +15,24 @@ public class TypeStream {
                 .filter(x -> x % 2 == 0) // Los filtra por datos pares
                 //.allMatch(x -> ) // funcion que retorna un boolean (Verifica que todos los valores sean validos)
                 .forEach(System.out::println); // Resp -> 0, 2, 4, 6 ...etc. hasta 998
+
+        getListNumbers();
+    }
+
+    private static void getListNumbers() {
+        IntStream infiniteStream = IntStream.iterate(0, x -> x + 1);
+        List<Integer> numberList = infiniteStream.limit(1000) // Hasta mil
+                .parallel() // Va regresando los resultados con forme van terminando (cuando no importa el orden de los datos)
+                .filter(x -> x % 2 == 0) // Los filtra por datos pares
+                //.allMatch(x -> ) // funcion que retorna un boolean (Verifica que todos los valores sean validos)
+                .boxed() // Convertir en un Stream de enteros
+                .collect(Collectors.toList()); // Construir un stream a una lista, o mapa, arreglot, etc,
+
+        // Collectors.toList() => Convierte a una lista
+        // Collectors.toCollection() => Permite generar una collection de datos
+        // Collectors.toConcurrentMap() => Para convertir un mapa concurrente para paralelismo o concurrencia
+        // Collectors.toMap() => Convierte a un map con una funcion
+        // Collectors.toSet() => Para hacer elementos unicos
     }
 
 }
